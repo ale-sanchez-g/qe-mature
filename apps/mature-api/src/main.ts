1a39e617e801fff1266b457a9f9cd3c877d6cd5a
Alejandro Sanchez Giraldo <alejandrosanchezgiraldo@Alejandros-MacBook-Air.local>
2021-04-22 23:27:46 +1000

    pug version

--- apps/mature-api/src/main.ts
+++ apps/mature-api/src/main.ts
@@ -4,15 +4,27 @@
  */
 
 import * as express from 'express';
-
+const pug = require('pug');
+// Compile the source code
+const compiledFunction = pug.compileFile('apps/mature-api/src/assets/home.pug');
 const app = express();
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to mature-api!' });
 });
 
+app.get('/', (req,res)=> {
+  res.send(compiledFunction({
+    name: 'QE-Mature'
+  }));
+});
+
+app.get('/org', (req,res)=> {
+  res.send({ message: 'Route to create Org!' });
+});
+
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
